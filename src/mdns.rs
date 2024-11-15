4b44ac12c2a73296a4616ba08a7bbbba28250af7
sergei_fatykov <sergey.fatykov@gmail.com>
2024-11-16 00:02:46 +0300

    Zeroconf renew.

--- src/mdns.rs
+++ src/mdns.rs
@@ -3,7 +3,9 @@
 use crate::devices::SharedDevices;
 use log::info;
 use std::net::IpAddr;
-use std::sync::Arc;
+use std::any::Any;
+use std::sync::{Arc, Mutex as StdMut};
+use std::time::Duration;
 
 use tokio::sync::Mutex;
 
@@ -11,17 +13,32 @@ use tokio::sync::Mutex;
 use {
     futures_util::{pin_mut, stream::StreamExt},
     mdns::{Record, RecordKind},
-    std::time::Duration,
 };
 
 #[cfg(feature = "zeroconf")]
 use {
     zeroconf::prelude::*,
-    zeroconf::{MdnsBrowser, ServiceType},
+    zeroconf::{MdnsBrowser, ServiceDiscovery, ServiceType},
 };
 
 const SERVICE_NAME: &str = "apple-mobdev2";
 const SERVICE_PROTOCOL: &str = "tcp";
+
+#[derive(Default, Debug, Clone)]
+pub struct Context {
+    name: String,
+    address: String,
+}
+
+#[cfg(feature = "zeroconf")]
+fn on_service_discovered(result: zeroconf::Result<ServiceDiscovery>, _context: Option<Arc<dyn Any>>,) {
+        if let Ok(service) = result {
+            info!("Service discovered: {:?}", service);
+            let mut context = _context.as_ref().unwrap().downcast_ref::<Arc<StdMut<Context>>>().unwrap().lock().unwrap();
+            context.name = String::from(service.name());
+            context.address = String::from(service.address());
+        }
+}
 
 #[cfg(feature = "zeroconf")]
 pub async fn discover(data: Arc<Mutex<SharedDevices>>) {
@@ -29,38 +46,46 @@ pub async fn discover(data: Arc<Mutex<SharedDevices>>) {
     println!("Starting mDNS discovery for {} with zeroconf", service_name);
 
     let mut browser = MdnsBrowser::new(ServiceType::new(SERVICE_NAME, SERVICE_PROTOCOL).unwrap());
+
+    browser.set_service_discovered_callback(Box::new(on_service_discovered));
+    browser.set_context(Box::new(Arc::new(StdMut::new(Context { name : String::from(""), address : String::from("")}))));
+
+    let event_loop = browser.browse_services().unwrap();
     loop {
-        let result = browser.browse_async().await;
+       event_loop.poll(Duration::from_secs(0)).unwrap();
 
-        if let Ok(service) = result {
-            info!("Service discovered: {:?}", service);
-            let name = service.name();
-            if !name.contains("@") {
-                continue;
-            }
-            let addr = match service.address() {
-                addr if addr.contains(":") => IpAddr::V6(addr.parse().unwrap()),
-                addr => IpAddr::V4(addr.parse().unwrap()),
-            };
+       let context = browser.context().unwrap().downcast_ref::<Arc<StdMut<Context>>>().unwrap().lock().unwrap();
+       let name = context.name.clone();
+       let address = context.address.clone();
+       info!("Name = {} ; Address = {}", name ,address); 
 
-            let mac_addr = name.split("@").collect::<Vec<&str>>()[0];
-            let mut lock = data.lock().await;
-            if let Ok(udid) = lock.get_udid_from_mac(mac_addr.to_string()) {
-                if lock.devices.contains_key(&udid) {
-                    info!("Device has already been added to muxer, skipping");
-                    continue;
-                }
-                println!("Adding device {}", udid);
+       if !name.contains("@") {
+               continue;
+       }
+ 
+       let addr = match address.clone() {
+               addr if addr.contains(":") => IpAddr::V6(addr.parse().unwrap()),
+               addr => IpAddr::V4(addr.parse().unwrap()),
+       };
 
-                lock.add_network_device(
-                    udid,
-                    addr,
-                    service_name.clone(),
-                    "Network".to_string(),
-                    data.clone(),
-                )
-            }
+       let mac_addr = name.split("@").collect::<Vec<&str>>()[0];
+       let mut lock = data.lock().await;
+
+       if let Ok(udid) = lock.get_udid_from_mac(mac_addr.to_string()) {
+           if lock.devices.contains_key(&udid) {
+               info!("Device has already been added to muxer, skipping");
+               return;
+           }
+           println!("Adding device {}", udid);
+           lock.add_network_device(
+               udid,
+               addr,
+               service_name.clone(),
+               "Network".to_string(),
+               data.clone(),
+           )
         }
+
     }
 }
 
